b5e0ac071aa09347edfc99d883ebbe3096a99fcb
Anthony Petrov <beavean@gmail.com>
2022-08-13 23:59:04 +0300

    refactor: Improve VideoViewController UI and methods for convenience

---
+++ The Movie Database/VideoView/VideoViewCell/VideoViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  VideoViewCell.swift
+//  The Movie Database
+//
+//  Created by Beavean on 13.08.2022.
+//
+
+import UIKit
+import youtube_ios_player_helper
+
+class VideoViewCell: UITableViewCell {
+    
+    @IBOutlet weak var videoPlayer: YTPlayerView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- The Movie Database/VideoView/VideoViewController.swift
+++ The Movie Database/VideoView/VideoViewController.swift
@@ -8,11 +8,6 @@
 import UIKit
 import youtube_ios_player_helper
 
-class VideoPlayerCell: UITableViewCell {
-    @IBOutlet weak var videoPlayer: YTPlayerView!
-    
-}
-
 class VideoViewController: UITableViewController {
     
     var mediaID: Int?
@@ -21,11 +16,12 @@ class VideoViewController: UITableViewController {
     var receivedDetails: [MediaVideos.Video]?
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UINib(nibName: "VideoViewCell", bundle: nil), forCellReuseIdentifier: "VideoViewCell")
         setupCompletions()
         viewModel.updateData()
-        tableView.reloadData()
     }
     
     func setupCompletions() {
@@ -33,11 +29,12 @@ class VideoViewController: UITableViewController {
         viewModel.mediaID = self.mediaID
         viewModel.onDataUpdated = { [weak self] in
             self?.receivedDetails = self?.viewModel.videoDetails
+            self?.tableView.reloadData()
         }
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         guard let videos = receivedDetails else { return 0 }
         return videos.count
@@ -45,58 +42,56 @@ class VideoViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "VideoPlayerCell", for: indexPath) as? VideoPlayerCell, let videos = receivedDetails else { return UITableViewCell() }
-        for video in videos {
-            guard let videoKey = video.key else { return UITableViewCell() }
-            cell.videoPlayer.load(withVideoId: videoKey, playerVars: ["playsinline": 1])
-        }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "VideoViewCell", for: indexPath) as? VideoViewCell, let videos = receivedDetails else { return UITableViewCell() }
+        guard let videoKey = videos.reversed()[indexPath.row].key else { return UITableViewCell() }
+        cell.videoPlayer.load(withVideoId: videoKey, playerVars: ["playsinline": 1])
         return cell
     }
-
-
+    
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
